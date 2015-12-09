b9efded2ddc3393a042ad8ab54d11b4bee707182
Nicolas Trauwaen <ntrauwaen@gmail.com>
2015-12-09 19:19:02 +0100

    add code to prepare automation

--- env.bat
+++ env.bat
@@ -1,3 +1,10 @@
+@echo off
 set DOCKER_TLS_VERIFY=1
 set DOCKER_CERT_PATH=C:\Users\<YOUR_USER>\.docker\machine\machines\default
 set DOCKER_HOST=tcp://192.168.99.100:2376
+
+REM set env automatically
+REM 2 known bugs :
+REM - DOCKER_CERT_PATH has to be set without quote
+REM - the last comment of the output of docker-machine env dev needs to be ignored
+REM for /f "tokens=2" %i in ('docker-machine env dev') do set %i
