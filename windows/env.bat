9e973a25fd1a178f91c00a12e7082963e7d0dc50
Nicolas Trauwaen <ntrauwaen@gmail.com>
2015-12-10 12:12:37 +0100

    updated automation thanks to docker-machine version 0.5.2 ( 0456b9f )

--- windows/env.bat
+++ windows/env.bat
@@ -1,10 +1,5 @@
 @echo off
-set DOCKER_TLS_VERIFY=1
-set DOCKER_CERT_PATH=C:\Users\<YOUR_USER>\.docker\machine\machines\default
-set DOCKER_HOST=tcp://192.168.99.100:2376
-
-REM set env automatically
-REM 2 known bugs :
-REM - DOCKER_CERT_PATH has to be set without quote
-REM - the last comment of the output of docker-machine env dev needs to be ignored
-REM for /f "tokens=2" %i in ('docker-machine env dev') do set %i
+REM requirement : docker-machine version 0.5.2 ( 0456b9f )
+REM %1 : name of your boot2docker VM
+REM Run this command to configure your shell:
+FOR /f "tokens=*" %i IN ('docker-machine env %1 --shell cmd') DO %i
