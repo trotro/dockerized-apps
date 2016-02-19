31d3d910b53b4e88661e800a0d05e12516bd0d2a
trotro <ntrauwaen@gmail.com>
2016-02-19 15:33:27 +0100

    moved to windows branch

--- windows/lynx.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM run lynx in a container
-REM based on https://github.com/jfrazelle/dotfiles/blob/master/.dockerfunc
-docker run --rm -it jess/lynx %*
-REM EOF
--- windows/ssh.ps1
+++
@@ -1,7 +0,0 @@
-REM based on https://hub.docker.com/r/sakai135/ssh-client/
-function ssh
-{
-    $cur = (Get-Item -Path ".\" -Verbose).FullName -replace '\\', '/' -replace '^C:/', '/c/'
-    $dir = $env:USERPROFILE -replace '\\', '/' -replace '^C:/', '/c/'
-    docker run --rm -it -v "$dir/.ssh`:/root/.ssh-copy" -v "$cur`:$cur" -w $cur sakai135/ssh-client ssh $args
-}
